6d69853c7bda557cd4ba06c70ff573d4e6c1fe58
Alina <linalevel5@gmail.com>
2022-12-13 15:27:54 +0300

    Initial Commit

---
+++ SpringAppHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAppHW
+//
+//  Created by MAcbook on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
